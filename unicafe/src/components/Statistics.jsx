95f22def789ffbf9daaaac602ccd9683bb2567ed
hummus-erectus <robert.samuel.grayson@gmail.com>
2023-03-22 01:08:23 +0900

    Complete 1.9

--- unicafe/src/components/Statistics.jsx
+++ unicafe/src/components/Statistics.jsx
@@ -5,12 +5,19 @@ const Statistics =({good, neutral, bad}) => {
     return (
         <>
             <h2>Statistics</h2>
-            <p>Good {good}</p>
-            <p>Neutral {neutral}</p>
-            <p>Bad {bad}</p>
-            <p>All {all}</p>
-            <p>Average {!all ? 0 : (good-bad)/all}</p>
-            <p>Positive {!all ? 0 : (good/all)*100}%</p>
+            {all ? (
+                <>
+                    <p>Good {good}</p>
+                    <p>Neutral {neutral}</p>
+                    <p>Bad {bad}</p>
+                    <p>All {all}</p>
+                    <p>Average {(good-bad)/all}</p>
+                    <p>Positive {(good/all)*100}%</p>
+                </>
+            )
+            :
+            <p>No feedback given</p>
+        }
         </>
     )
 }
